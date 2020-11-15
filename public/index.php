2345bb1b1cc386f398858c526ff5c7087ab0e3a3
Celebro01 <dangerveneta@gmail.com>
2020-11-14 21:00:28 -0300

    adicionando primeiro modelo_logico e algumas formatações css

--- public/index.php
+++ public/index.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>I Don't know</title>
+    <title>Gamesow-Clone</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="src/fontawesome/css/all.css">
     <link rel="stylesheet" href="src/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link rel="shortcut icon" href="src/image/icon.jpg" >
+
+
 <style>
    
     
@@ -17,7 +21,6 @@
 </head>
 <body>
     <!-- Navbar -->
-
     <header class="container" id="header">
         <nav class="navbar navbar-dark navbar-expand-sm">
             <a href="" class="navbar-brand"><img src="http://static.gamesow.com/br/images/logo.png" alt=""></a>
@@ -27,6 +30,7 @@
             <div class="collapse navbar-collapse" id="menu-principal">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item"><a href="" class="nav-link">Login</a></li>
+                    <li class="nav-item divisao"><a href="" class="nav-link"></a></li>
                     <li class="nav-item"><a href="" class="nav-link">Registrar</a></li>
                 </ul>
             </div>
@@ -47,7 +51,7 @@
             </div>
 
             <div class="carousel-item">
-                <img src="src/image/banner-5.png" class="d-block w-100" alt="...">
+                <img src="src/image/banner-5.jpg" class="d-block w-100" alt="...">
             </div>
 
             <div class="carousel-item">
@@ -68,21 +72,23 @@
 
 
     </div>
-
-    <main class="container-lg mt-4">
+    <!-- Conteudo principal -->
+    <main class="container-lg my-4">
         <div class="row  px-4">
             <!-- PRINCIPAL -->
             <div class="col-md-8 principal mb-4">
                 <!-- Item -->
                 <div class="row item-jogo">
+                    <!-- Banner -->
                     <div class="col-lg-6 col-md-6 col-sm-6 col-12 p-0 border-esquerda">
                         <img src="https://img.gamesow.com/image/2018/0309/1520575930.jpg" alt="" class="w-100">
                     </div>
+                    <!-- Descrição -->
                     <div class="col-lg-6  col-md-6 col-sm-6 col-12 d-flex flex-column justify-content-center align-items-center bg-info text-light p-0 border-direita">
                         <h2>Asa de Cristal</h2>
                         <p>Aqui vai começar a sua jornada!</p>
                     </div>
-
+                    <!-- links -->
                     <div class="links d-flex align-items-center justify-content-center">
                        <div class="">
                         <a href="google" class="btn btn-outline-light  p-3"> <i class="fas fa-home"></i></a>
@@ -97,7 +103,7 @@
                     <div class=" col-lg-6 col-md-6 col-sm-6 col-12 p-0 order-last border-direita">
                         <img src="https://img.gamesow.com/image/2016/1117/1479434557.jpg" alt="" class="w-100">
                     </div>
-                    <div class="col-lg-6  col-md-6 col-sm-6 col-12 d-flex flex-column justify-content-center align-items-center bg-info text-light p-0 order-first border-esquerda">
+                    <div class="col-lg-6  col-md-6 col-sm-6 col-12 d-flex flex-column justify-content-center align-items-center bg-dark text-light p-0 order-first border-esquerda">
                         <h2>Asa de Cristal</h2>
                         <p>Aqui vai começar a sua jornada!</p>
                     </div>
@@ -136,7 +142,7 @@
                     <div class="col-lg-6 col-md-6 col-sm-6 col-12 p-0 order-last border-direita">
                         <img src="https://img.gamesow.com/image/2016/0308/1457436842.jpg" alt="" class="w-100">
                     </div>
-                    <div class="col-lg-6  col-md-6 col-sm-6 col-12 d-flex flex-column justify-content-center align-items-center bg-info text-light p-0 order-first border-esquerda">
+                    <div class="col-lg-6  col-md-6 col-sm-6 col-12 d-flex flex-column justify-content-center align-items-center bg-dark text-light p-0 order-first border-esquerda">
                         <h2>Asa de Cristal</h2>
                         <p>Aqui vai começar a sua jornada!</p>
                     </div>
@@ -168,44 +174,42 @@
                         <img src="https://img.gamesow.com/image/2015/0701/1435733586.jpg" alt="" width="100%" class="rounded">
                     </div>
                 </div>
-                <!-- <ul class="list-group">
-                    <li class="list-item item-lateral"><img src="https://img.gamesow.com/image/2016/1227/1482832598.jpg" alt="" width="100%" class="rounded"></li>
-                    <li class="list-item item-lateral"><img src="https://img.gamesow.com/image/2016/1227/1482832598.jpg" alt="" width="100%" class="rounded"></li>
-                    <li class="list-item item-lateral"><img src="https://img.gamesow.com/image/2016/1227/1482832598.jpg" alt="" width="100%" class="rounded"></li>
-                    <li class="list-item item-lateral"><img src="https://img.gamesow.com/image/2016/1227/1482832598.jpg" alt="" width="100%" class="rounded"></li>
-                    <li class="list-item item-lateral"><img src="https://img.gamesow.com/image/2016/1227/1482832598.jpg" alt="" width="100%" class="rounded"></li>
-                </ul> -->
             </div>
         </div>
     </main>
-
-    <footer class="jumbotron m-0">
-        <div class="container-lg ">
-            <div class="row justify-content-between">
-                <div class="col-md-6 footer-area-links1">
-                    <h2>GAMESOW</h2>
-
-                    <a href="" class="nav-link foot-link">Fórum</a>
-                    <a href="" class="nav-link foot-link">Facebook</a>
-                    <a href="" class="nav-link foot-link">Luz da Escuridão</a>
-                    <a href="" class="nav-link foot-link">Asa de Cristal</a>
-                    <a href="" class="nav-link foot-link">Gamesow Espanõl</a>
+    <!-- FOOTER -->
+    <footer>
+        <!-- AREA 1 --> 
+        <div class="jumbotron m-0">
+            <div class="container-lg ">
+                <div class="row justify-content-between">
+                    <div class="col-md-6 footer-area-links1 ">
+                        <h2>GAMESOW</h2>
+    
+                        <a href="" class="nav-link foot-link">Fórum</a>
+                        <a href="" class="nav-link foot-link">Facebook</a>
+                        <a href="" class="nav-link foot-link">Luz da Escuridão</a>
+                        <a href="" class="nav-link foot-link">Asa de Cristal</a>
+                        <a href="" class="nav-link foot-link">Gamesow Espanõl</a>
+                    </div>
+                    <div class="col-md-5">
+                        <h2>CORPORATE</h2>
+    
+                        <a href="" class="nav-link foot-link">FAQ</a>
+                        <a href="" class="nav-link foot-link">Politica de Reembolso</a>
+                        <a href="" class="nav-link foot-link">Politica de Privacidade</a>
+                        <a href="" class="nav-link foot-link">Sobre nós</a>
+                    </div>
                 </div>
-                <div class="col-md-5">
-                    <h2>CORPORATE</h2>
-
-                    <a href="" class="nav-link foot-link">FAQ</a>
-                    <a href="" class="nav-link foot-link">Politica de Reembolso</a>
-                    <a href="" class="nav-link foot-link">Politica de Privacidade</a>
-                    <a href="" class="nav-link foot-link">Sobre nós</a>
-                </div>
+    
             </div>
-
+        </div>
+        <!-- AREA 2 -->
+        <div class="d-flex justify-content-center  py-4">
+            <h5 class="lead"style="color:gray;" >COPYRIGHT © 2015 GAMESOW & ENTERTAINMENT, INC.</h5> 
+    
         </div>
     </footer>
-    <div class="d-flex justify-content-center  py-4">
-        <h5 class="lead"style="color:gray;" >COPYRIGHT © 2015 GAMESOW & ENTERTAINMENT, INC.</h5> 
 
-    </div>
 </body>
 </html>
