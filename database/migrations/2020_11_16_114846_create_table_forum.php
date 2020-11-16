b1ececfc60156b7663b49c5868c3e8ec027d8ac1
Celebro01 <dangerveneta@gmail.com>
2020-11-16 09:18:13 -0300

    criando primeira migration para o forum

---
+++ database/migrations/2020_11_16_114846_create_table_forum.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableForum extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forum', function (Blueprint $table) {
+            $table->id();
+            $table->string('jogo');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_forum');
+    }
+}
