3b89b812a4618d32898484aa4efacf820d371f8f
Celebro01 <dangerveneta@gmail.com>
2020-11-20 18:05:38 -0300

    Renomeando Rotas

---
+++ database/migrations/2020_11_20_164256_create_administrador_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdministradorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrador', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email');
+            $table->string('senha');
+            $table->unsignedBigInteger('fk_id_jogo');
+            $table->foreign('fk_id_jogo')->references('id')->on('jogos');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrador');
+    }
+}
