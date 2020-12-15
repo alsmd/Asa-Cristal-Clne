c18cc7876150af494d4b3d57db5d6369b0d2df1d
Celebro01 <dangerveneta@gmail.com>
2020-12-15 08:46:11 -0300

    baixando jquery e bootstrap diretamente no projeto

--- resources/views/layouts/layout2.blade.php
+++ resources/views/layouts/layout2.blade.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gamesow-Clone</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!--Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost:8080/src/css/bootstrap.min.css" >
     <script
-    src="https://code.jquery.com/jquery-3.5.1.js"
-    integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-    crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    src="http://localhost:8080/src/js/jquery-3.5.1.js"
+    ></script>
+    <script src="http://localhost:8080/src/js/popper.min.js" ></script>
+    <script src="http://localhost:8080/src/js/bootstrap.min.js" ></script>
+    
     <!--FontAwesome -->
     <link rel="stylesheet" href="http://localhost:8080/src/fontawesome/css/all.css">
     <link rel="preconnect" href="https://fonts.gstatic.com"> 
